d43070f9ce928d8fdd37b58575afc837e1688ad4
Isaac0nchain <alfredisaac2007@gmail.com>
2025-10-07 01:37:55 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Isaac', // Your name
+  botOwner: 'Ace', // Your name
   ownerNumber: '+2348155297180', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
